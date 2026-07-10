6dc8fb493ad43762d0bdc424211223b9c2bee7d5
marianohilario <marianohilario@gmail.com>
2026-07-09 21:27:19 -0300

    feat: doy funcionalidad al boton de whatsapp

--- src/components/whatsApp/WhatsApp.jsx
+++ src/components/whatsApp/WhatsApp.jsx
@@ -2,9 +2,15 @@ import styles from "./whatsApp.module.css";
 
 const WhatsApp = () => {
   return (
-    <div className={styles.whatsApp}>
+    <a
+      href="https://wa.me/5491151339874"
+      className={styles.whatsApp}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Contactar por WhatsApp"
+    >
       <i className="bi bi-whatsapp"></i>
-    </div>
+    </a>
   );
 };
 

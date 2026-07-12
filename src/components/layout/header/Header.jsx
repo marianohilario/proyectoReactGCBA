4333d3bfbd1ed0e59540a2c5980e86f528035664
marianohilario <marianohilario@gmail.com>
2026-07-12 16:21:14 -0300

    feat: agrego validacion de usuario en header

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
+import { useAuth } from "../../../context/AuthContext";
 import CartIcon from "../../cart/CartIcon";
+import User from "../../user/User";
 import styles from "./header.module.css";
 
 const Header = () => {
+  const { user } = useAuth();
+
   return (
     <header className={styles.header}>
       <CartIcon />
+      {user ? (
+        <User />
+      ) : (
+        <Link to="/login" className={styles.loginLink}>
+          Login
+        </Link>
+      )}
     </header>
   );
 };

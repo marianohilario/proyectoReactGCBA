7e20e12e0692f3ed8eae1c3a5e73244ebeee6f8f
marianohilario <marianohilario@gmail.com>
2026-07-13 09:34:52 -0300

    feat: agrego register en navbar

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "../../../context/AuthContext";
 import CartIcon from "../../cart/CartIcon";
 import User from "../../user/User";
 import styles from "./header.module.css";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const { user, logout } = useAuth();
-  console.log("Header user:", user);
+  const location = useLocation();
+  const [activeRoute, setActiveRoute] = useState(location.pathname);
+
+  useEffect(() => {
+    setActiveRoute(location.pathname);
+  }, [location.pathname]);
 
   return (
     <header className={styles.header}>
@@ -14,9 +20,20 @@ const Header = () => {
       {user ? (
         <User />
       ) : (
-        <Link to="/login" className={styles.loginLink}>
-          Login
-        </Link>
+        <div className={styles.authLinks}>
+          <Link
+            to="/register"
+            className={`${styles.authLink} ${activeRoute === "/register" ? styles.active : ""}`}
+          >
+            Registrarse
+          </Link>
+          <Link
+            to="/login"
+            className={`${styles.authLink} ${activeRoute === "/login" ? styles.active : ""}`}
+          >
+            Login
+          </Link>
+        </div>
       )}
     </header>
   );
